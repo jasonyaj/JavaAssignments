ac9b19f03d6485a462361b0d7727a1e548c7bb09
jasonyaj <jason.yaj@gmail.com>
2023-06-05 16:59:51 -0500

    CafeJava assignment

--- Fundamentals/CafeJava/CafeJava.java
+++ Fundamentals/CafeJava/CafeJava.java
@@ -9,16 +9,48 @@ public class CafeJava {
         
         // Menu variables (add yours below)
         double mochaPrice = 3.5;
-    
+        double dripCoffeePrice = 4.00;
+        double lattePrice = 4.25;
+        double cappuccinoPrice = 5.00;
+
         // Customer name variables (add yours below)
         String customer1 = "Cindhuri";
+        String customer2 = "Sam";
+        String customer3 = "Jimmy";
+        String customer4 = "Noah";
     
         // Order completions (add yours below)
-        boolean isReadyOrder1 = false;
+        boolean isReadyOrder1 = true;
+        boolean isReadyOrder2 = true;
+        boolean isReadyOrder3 = true;
+        boolean isReadyOrder4 = true;
     
         // APP INTERACTION SIMULATION (Add your code for the challenges below)
         // Example:
         System.out.println(generalGreeting + customer1); // Displays "Welcome to Cafe Java, Cindhuri"
     	// ** Your customer interaction print statements will go here ** //
+        
+        //Cindhuri just ordered coffee
+        System.out.println(pendingMessage + customer1); 
+        
+        // Noad ordered cappuccino, (using if statements to display query of order)
+        if(isReadyOrder4) {
+            System.out.println(customer4 + readyMessage + displayTotalMessage + cappuccinoPrice);
+        }
+        else {
+            System.out.println(customer4 + pendingMessage);
+        }
+
+        // Sam just ordered 2 lattes, print the message to display their total. Next, use an if statement to print the appropriate order status message. Change the isReady flag value from true to false or vice versa in order to test your control logic (if-statement).
+        System.out.println(displayTotalMessage + lattePrice*2);
+        if(isReadyOrder2) {
+            System.out.println(customer2 + readyMessage);
+        }
+        else {
+            System.out.println(customer2 + pendingMessage);
+        }
+
+        // Jimmy just realized he was charged for a coffee but had ordered a latte. Print the total message with the new calculated total (what he owes) to make up the difference.
+        System.out.println(displayTotalMessage + (lattePrice - dripCoffeePrice));
     }
 }
